17dd92ff71799ee9ead9a4f5d67ef3972f009613
Guilber Soares <soaresguilber@gmail.com>
2024-09-12 15:55:59 -0300

    Commit #2: Statics e templates.

---
+++ projetoIntegrador/src/main/java/com/health/projetoIntegrador/controller/viewController.java
@@ -0,0 +1,22 @@
+package com.health.projetoIntegrador.controller;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+/**
+ *
+ * @author soare
+ */
+@Controller
+public class viewController {
+    
+    @RequestMapping("/")
+    public String index(){
+        return "homepage";
+    }
+    
+    @RequestMapping(method=RequestMethod.GET, value="/cadastro")
+    public String telaCadastro(){
+        return "cadastro";
+    }
+}
---
+++ projetoIntegrador/src/main/java/com/health/projetoIntegrador/model/User.java
@@ -0,0 +1,29 @@
+
+package com.health.projetoIntegrador.model;
+
+/**
+ *
+ * @author soare
+ */
+public class User {
+    String username;
+    String password; 
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String Username) {
+        this.username = Username;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+    
+    
+}
